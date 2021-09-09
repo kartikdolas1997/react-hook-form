93219b1a3abfad378de9a2055990694b8abc0ebd
kartikdolas <kartik.dolas@valuelabs.com>
2021-09-09 22:16:48 +0530

    till cascaded dropdown

--- src/Components/Validations.js
+++ src/Components/Validations.js
@@ -4,11 +4,9 @@ import { useForm } from "react-hook-form";
 import classNames from "classnames";
 import arr from "../../src/data"
 
-const getCity = (evt) =>{
-  console.log(evt.target.value);
-  console.log(arr);
 
-}
+let finalCities = [];
+
 
 
 function Validations() {
@@ -23,6 +21,20 @@ function Validations() {
   const onSubmit = (data) => {
     console.log(data);
   };
+
+  const getCity = (evt) => {
+    console.log(evt.target.value);
+
+
+    arr.forEach(x => {
+      if (x.state == evt.target.value) {
+        console.log(x.cities);
+        finalCities = x.cities
+      }
+    })
+
+  }
+
 
 
 
@@ -161,23 +173,25 @@ function Validations() {
               </div>
               <div className="form-group">
                 <select className="custom-select" name="state" {...register("address.state")} onChange={getCity}>
-                  <option value="">Select your state</option>
+                  {/* <option value="">Select your state</option>
                   <option value="Delhi">Delhi</option>
                   <option value="Punjab">Punjab</option>
                   <option value="Jharkhand">Jharkhand</option>
-                  <option value="Bihar">Bihar</option>
+                  <option value="Bihar">Bihar</option> */}
+                  {arr.map((e, key) => {
+                    return <option key={key} value={e.value}>{e.state}</option>;
+                  })}
                 </select>
               </div>
 
+              {/* finalCities */}
               <div className="form-group">
                 <label htmlFor="City">City</label>
-                <input
-                  type="text"
-                  className="form-control"
-                  id="city"
-                  name="address.city"
-                  {...register("address.city")}
-                />
+                <select className="custom-select" name="address.city" {...register("address.city")}>
+                  {finalCities.map((e) => {
+                    return <option key={e} value={e}>{e}</option>;
+                  })}
+                </select>
               </div>
 
               <div className="form-group">
